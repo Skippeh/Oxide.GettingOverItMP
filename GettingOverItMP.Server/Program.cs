ed1dbe187d1caa3a4d58474f53c792da9062d4b7
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-12-07 13:11:16 +0100

    Replaced shared project with .net standard targeting net35 and net standard 2.0

--- GettingOverItMP.Server/Program.cs
+++ GettingOverItMP.Server/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace GettingOverItMP.Server
+﻿using System;
+using System.Threading;
+using ServerShared;
+
+namespace GettingOverItMP.Server
 {
     public static class Program
     {
+        private static GameServer server;
+
         private static void Main(string[] args)
         {
+            server = new GameServer();
+            server.Start();
 
+            while (!Console.KeyAvailable)
+            {
+                server.Update();
+                Thread.Sleep(100);
+            }
         }
     }
 }
---
+++ ServerShared/GameServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LiteNetLib;
+
+namespace ServerShared
+{
+    public class GameServer
+    {
+        private EventBasedNetListener listener;
+        private NetManager server;
+
+        public GameServer(int maxConnections)
+        {
+            if (maxConnections <= 0)
+                throw new ArgumentException("Max connections needs to be > 0.");
+
+            server = new NetManager(listener, maxConnections, "GOIMP");
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
