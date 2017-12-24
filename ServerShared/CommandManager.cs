af75987cb528b1126acf98115a50a22e85706a0c
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-12-24 14:25:13 +0100

    Added CommandParser library and replaced old parsing code

--- ServerShared/CommandManager.cs
+++ ServerShared/CommandManager.cs
@@ -2,70 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Pyratron.Frameworks.Commands.Parser;
 
 namespace ServerShared
 {
     public class CommandManager
     {
         private GameServer server;
+        private CommandParser parser;
 
         public CommandManager(GameServer server)
         {
             this.server = server;
+            parser = CommandParser.CreateNew(prefix: "/");
         }
 
-        public void HandleChatMessage(string message)
+        public bool HandleChatMessage(string message)
         {
-            string[] words = message.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-            string command = words[0];
-            string[] args = ParseArgs(words.Skip(1));
-
-            /*Console.WriteLine($"Raw: {message}");
-            Console.WriteLine($"Raw args: '{string.Join("', '", words.Skip(1).ToArray())}'");
-            Console.WriteLine($"Command: \"{command}\"");
-            Console.WriteLine($"Args ({args.Length}): '{string.Join("', '", args)}'");*/
-        }
-
-        private string[] ParseArgs(IEnumerable<string> wordsEnumerable)
-        {
-            var result = new List<string>();
-            string[] words = wordsEnumerable.ToArray();
-
-            StringBuilder currentWord = null;
-            for (int i = 0; i < words.Length; ++i)
-            {
-                string word = words[i];
-                string cleanWord = word.Replace("\"", "");
-
-                if (cleanWord == string.Empty && currentWord == null)
-                {
-                    result.Add(cleanWord);
-                    continue;
-                }
-
-                if (currentWord == null)
-                {
-                    currentWord = new StringBuilder(cleanWord);
-
-                    if (!word.StartsWith("\""))
-                    {
-                        result.Add(currentWord.ToString());
-                        currentWord = null;
-                    }
-                }
-                else
-                {
-                    currentWord.Append(" " + cleanWord);
-
-                    if (word.EndsWith("\""))
-                    {
-                        result.Add(currentWord.ToString());
-                        currentWord = null;
-                    }
-                }
-            }
-
-            return result.ToArray();
+            return parser.Parse(message);
         }
     }
 }
--- ServerShared/GameServer.cs
+++ ServerShared/GameServer.cs
@@ -478,77 +478,74 @@ namespace ServerShared
                         if (message.Length > SharedConstants.MaxChatLength)
                             message = message.Substring(0, SharedConstants.MaxChatLength);
 
-                        // Todo: better handling of chat commands
-                        if (message.StartsWith("/"))
+                        if (Commands.HandleChatMessage(message))
+                            return;
+
+                        #region old
+                        /*if (message.StartsWith("/spectate "))
                         {
-                            Commands.HandleChatMessage(message.Substring(1));
-
-                            /*if (message.StartsWith("/spectate "))
+                            NetPlayer target;
+                            int targetId;
+                            if (!int.TryParse(message.Substring("/spectate ".Length), out targetId))
                             {
-                                NetPlayer target;
-                                int targetId;
-                                if (!int.TryParse(message.Substring("/spectate ".Length), out targetId))
+                                var players = Players.Values.Where(plr => !plr.Spectating && plr.Name.ToLower().StartsWith(message.Substring("/spectate ".Length).ToLower())).ToList();
+
+                                if (players.Count == 0)
                                 {
-                                    var players = Players.Values.Where(plr => !plr.Spectating && plr.Name.ToLower().StartsWith(message.Substring("/spectate ".Length).ToLower())).ToList();
-
-                                    if (players.Count == 0)
-                                    {
-                                        peerPlayer.SendChatMessage("There is no player with this name.", SharedConstants.ColorRed);
-                                        return;
-                                    }
-
-                                    if (players.Count > 1)
-                                    {
-                                        peerPlayer.SendChatMessage("Found more than 1 player with this name. Try be more specific or type their id instead.", SharedConstants.ColorRed);
-                                        return;
-                                    }
-
-                                    target = players.First();
-                                }
-                                else
-                                {
-                                    target = Players.Values.FirstOrDefault(plr => !plr.Spectating && plr.Id == targetId);
-
-                                    if (target == null)
-                                    {
-                                        peerPlayer.SendChatMessage("There is no player with this id.", SharedConstants.ColorRed);
-                                        return;
-                                    }
-                                }
-
-                                if (target == peerPlayer)
-                                {
-                                    peerPlayer.SendChatMessage("You can't spectate yourself dummy.", SharedConstants.ColorRed);
+                                    peerPlayer.SendChatMessage("There is no player with this name.", SharedConstants.ColorRed);
                                     return;
                                 }
 
-                                peerPlayer.Spectate(target);
+                                if (players.Count > 1)
+                                {
+                                    peerPlayer.SendChatMessage("Found more than 1 player with this name. Try be more specific or type their id instead.", SharedConstants.ColorRed);
+                                    return;
+                                }
+
+                                target = players.First();
                             }
-                            else if (message.StartsWith("/shrug"))
+                            else
                             {
-                                string prefix = "";
+                                target = Players.Values.FirstOrDefault(plr => !plr.Spectating && plr.Id == targetId);
 
-                                if (message.Length > "/shrug ".Length)
-                                    prefix = message.Substring("/shrug ".Length);
-
-                                prefix = prefix.Trim();
-
-                                BroadcastChatMessage($"{prefix} ¯\\_(ツ)_/¯", Color.white, peerPlayer);
+                                if (target == null)
+                                {
+                                    peerPlayer.SendChatMessage("There is no player with this id.", SharedConstants.ColorRed);
+                                    return;
+                                }
                             }
-                            else if (message.StartsWith("/tableflip"))
+
+                            if (target == peerPlayer)
                             {
-                                string prefix = "";
+                                peerPlayer.SendChatMessage("You can't spectate yourself dummy.", SharedConstants.ColorRed);
+                                return;
+                            }
 
-                                if (message.Length > "/tableflip ".Length)
-                                    prefix = message.Substring("/tableflip ".Length);
-
-                                prefix = prefix.Trim();
-
-                                BroadcastChatMessage($"{prefix} (╯°□°）╯︵ ┻━┻", Color.white, peerPlayer);
-                            }*/
-
-                            return;
+                            peerPlayer.Spectate(target);
                         }
+                        else if (message.StartsWith("/shrug"))
+                        {
+                            string prefix = "";
+
+                            if (message.Length > "/shrug ".Length)
+                                prefix = message.Substring("/shrug ".Length);
+
+                            prefix = prefix.Trim();
+
+                            BroadcastChatMessage($"{prefix} ¯\\_(ツ)_/¯", Color.white, peerPlayer);
+                        }
+                        else if (message.StartsWith("/tableflip"))
+                        {
+                            string prefix = "";
+
+                            if (message.Length > "/tableflip ".Length)
+                                prefix = message.Substring("/tableflip ".Length);
+
+                            prefix = prefix.Trim();
+
+                            BroadcastChatMessage($"{prefix} (╯°□°）╯︵ ┻━┻", Color.white, peerPlayer);
+                        }*/
+                        #endregion
 
                         Color color = Color.white;
                         BroadcastChatMessage(message, color, peerPlayer);
