b4c213b871a388438f679c491ddbb6308dadd0b6
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-12-26 16:01:31 +0100

    Added ban command

---
+++ ServerShared/ChatCommands/BanCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Pyratron.Frameworks.Commands.Parser;
+using ServerShared.Player;
+
+namespace ServerShared.ChatCommands
+{
+    [ChatCommand("Ban", "ban", "Ban the specified player for an optional amount of time in minutes.")]
+    public class BanCommand : ChatCommand
+    {
+        [CommandArgument("Player name")]
+        public string PlayerName { get; set; }
+
+        [CommandArgument("Reason")]
+        public string Reason { get; set; }
+
+        [CommandArgument("Ban length", true, defaultValue: "0")]
+        public int Minutes { get; set; }
+
+        public override void Handle(NetPlayer caller, string[] args)
+        {
+            var netPlayer = Server.Players.Values.FirstOrDefault(plr => plr.Name.ToLower().StartsWith(PlayerName.ToLower()));
+
+            if (netPlayer == null)
+            {
+                caller.SendChatMessage($"Could not find a player with a name starting with '{PlayerName}'.", SharedConstants.ColorRed);
+                return;
+            }
+
+            if (Minutes < 0)
+            {
+                caller.SendChatMessage("Invalid time specified (needs to be 0 or higher).", SharedConstants.ColorRed);
+                return;
+            }
+
+            if (netPlayer == caller)
+            {
+                caller.SendChatMessage("You can't ban yourself.", SharedConstants.ColorRed);
+                return;
+            }
+
+            string suffix;
+
+            if (Minutes > 0)
+                suffix = $"for {Minutes} minute(s)";
+            else
+                suffix = "permanently";
+
+            Server.BanPlayer(netPlayer, Reason, DateTime.UtcNow.AddMinutes(Minutes));
+            Server.BroadcastChatMessage($"{netPlayer.Name} was banned {suffix}.", SharedConstants.ColorBlue);
+        }
+    }
+}
