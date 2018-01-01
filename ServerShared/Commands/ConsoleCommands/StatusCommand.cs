8fc1ef6a7fcbb86f3ae1bdfe1c6c4a0f00cb2d31
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-01 23:17:11 +0100

    Added 'status' console command

---
+++ ServerShared/Commands/ConsoleCommands/StatusCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using Pyratron.Frameworks.Commands.Parser;
+using ServerShared.Logging;
+
+namespace ServerShared.Commands.ConsoleCommands
+{
+    [Command("Status", "status", "Show server status and connected players.")]
+    public class StatusCommand : ConsoleCommand
+    {
+        public override void Handle(string[] args)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine($"Host: {Server.Peer.Configuration.LocalAddress}:{Server.Port}");
+            builder.AppendLine($"Server name: {Server.Name}");
+            builder.AppendLine($"Connected players: {Server.Players.Count}/{Server.MaxPlayers}");
+
+            if (Server.Players.Count > 0)
+            {
+                foreach (var player in Server.Players.Values)
+                {
+                    builder.AppendLine($"- {player.Name} ({player.Identity})");
+                }
+            }
+
+            SendMessage(builder.ToString(), LogMessageType.Info);
+        }
+    }
+}
