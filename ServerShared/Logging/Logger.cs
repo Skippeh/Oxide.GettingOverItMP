f255e32d73209bda42ce50af7d4c71fd5bf3a560
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-01 20:18:33 +0100

    Refactored Logger a bit to add ability to specify LogMessageType manually.

--- ServerShared/Logging/Logger.cs
+++ ServerShared/Logging/Logger.cs
@@ -8,32 +8,54 @@ namespace ServerShared.Logging
 
         public static void LogInfo(object msg)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Info, msg));
+            Log(msg, LogMessageType.Info);
         }
 
         public static void LogDebug(object msg)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Debug, msg));
+            Log(msg, LogMessageType.Debug);
         }
 
         public static void LogWarning(object msg)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Warning, msg));
+            Log(msg, LogMessageType.Warning);
         }
 
         public static void LogError(object msg)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Error, msg));
+            Log(msg, LogMessageType.Error);
         }
 
         public static void LogException(object msg, Exception ex)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Exception, msg, ex));
+            Log(msg, LogMessageType.Exception, ex);
         }
 
         public static void LogException(Exception ex)
         {
-            LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Exception, string.Empty, ex));
+            Log(null, LogMessageType.Info, ex);
+        }
+
+        public static void Log(object msg, LogMessageType type, Exception ex = null)
+        {
+            switch (type)
+            {
+                case LogMessageType.Info:
+                    LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Info, msg));
+                    break;
+                case LogMessageType.Debug:
+                    LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Debug, msg));
+                    break;
+                case LogMessageType.Warning:
+                    LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Warning, msg));
+                    break;
+                case LogMessageType.Error:
+                    LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Error, msg));
+                    break;
+                case LogMessageType.Exception:
+                    LogMessageReceived?.Invoke(new LogMessageReceivedEventArgs(LogMessageType.Exception, msg ?? string.Empty, ex));
+                    break;
+            }
         }
     }
 
