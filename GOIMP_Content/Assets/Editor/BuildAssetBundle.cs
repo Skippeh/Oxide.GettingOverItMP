a40ddfcfe457063407a2000a76c33200915c011d
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-02 00:48:55 +0100

    Added unity project with base asset bundle setup

---
+++ GOIMP_Content/Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundle : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = Path.Combine(Application.dataPath, @"AssetBundles");
+
+        Debug.Log("Building asset bundles to " + assetBundleDirectory);
+
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
