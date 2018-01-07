6a834bc9ef9ae19d491673f3d6e6c90001574323
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-07 16:22:33 +0100

    Added chat command for setting pot color

--- ServerShared/Commands/ChatCommands/CheatCommands.cs
+++ ServerShared/Commands/ChatCommands/CheatCommands.cs
@@ -46,4 +46,51 @@ namespace ServerShared.Commands.ChatCommands
             target.SetGoldness(Goldness);
         }
     }
+
+    [Command("Set pot color", "potcolor", "Set yours or someone elses pot's color.")]
+    [RequireAuth(AccessLevel.Moderator)]
+    public class SetPotColorChatCommand : ChatCommand
+    {
+        [CommandArgument("Red (0-1)")]
+        public float R { get; set; }
+
+        [CommandArgument("Green (0-1)")]
+        public float G { get; set; }
+
+        [CommandArgument("Blue (0-1)")]
+        public float B { get; set; }
+
+        [CommandArgument("Player name", optional: true, defaultValue: null)]
+        public string PlayerName { get; set; }
+
+        public override void Handle(string[] args)
+        {
+            NetPlayer target = Caller;
+
+            if (PlayerName != null)
+            {
+                target = Server.FindPlayer(PlayerName, NameSearchOption.StartsWith);
+
+                if (target == null)
+                {
+                    SendMessage($"Could not find a player with a name starting with '{PlayerName}'.", LogMessageType.Error);
+                    return;
+                }
+            }
+
+            if (target == null)
+            {
+                SendMessage("Missing player name, if typed in server console a player name needs to be specified.", LogMessageType.Error);
+                return;
+            }
+
+            if (target.Spectating)
+            {
+                SendMessage($"Can't set pot color on spectating players.", LogMessageType.Error);
+                return;
+            }
+
+            target.SetPotColor(new Color(R, G, B));
+        }
+    }
 }
