8f0cca7cec2fa061623e768f07c969c72573ea9d
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-07 16:53:41 +0100

    Assume that server version is the currently released version (at the time of this commit)

--- ServerShared/Networking/DiscoveryServerInfo.cs
+++ ServerShared/Networking/DiscoveryServerInfo.cs
@@ -10,6 +10,6 @@ namespace ServerShared.Networking
         public ushort Players;
         public ushort MaxPlayers;
         public List<string> PlayerNames = new List<string>();
-        public int ServerVersion;
+        public int ServerVersion = 8;
     }
 }
