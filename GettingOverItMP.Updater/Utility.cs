edbcad2875d761789315c65eb825d7774810d5e3
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-14 15:36:30 +0100

    Added Utility class with method to get file hash (md5)

---
+++ GettingOverItMP.Updater/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace GettingOverItMP.Updater
+{
+    internal static class Utility
+    {
+        public static string GetFileHash(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return null;
+
+            using (var md5 = MD5.Create())
+            {
+                using (var fileStream = File.OpenRead(filePath))
+                {
+                    var hashBytes = md5.ComputeHash(fileStream);
+                    return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+    }
+}
