1140def05b9aef1849e103afd2d019abd69d0396
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-15 23:32:18 +0100

    Simplified BasicUserValidator

--- WebAPI/Authentication/BasicUserValidator.cs
+++ WebAPI/Authentication/BasicUserValidator.cs
@@ -9,12 +9,11 @@ namespace WebAPI.Authentication
     {
         public ClaimsPrincipal Validate(string username, string password)
         {
-            var account = Data.UserCredentials.FirstOrDefault(credentials => credentials.Username.ToLowerInvariant() == username.ToLowerInvariant() && credentials.Password == password);
+            UserCredential account = Data.UserCredentials.FirstOrDefault(credentials => credentials.Username.ToLowerInvariant() == username.ToLowerInvariant() && credentials.Password == password);
+
             if (account != null)
             {
-                var principal = new ClaimsPrincipal(new GenericIdentity(account.Username, "Basic"));
-                var claimsPrincipal = principal;
-                return claimsPrincipal;
+                return new ClaimsPrincipal(new GenericIdentity(account.Username, "Basic"));
             }
 
             return null;
