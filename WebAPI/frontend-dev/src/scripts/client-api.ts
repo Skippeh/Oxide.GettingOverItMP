1cbbf55a75da9ffc7f186871e61d2b240e0c30d7
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-01-20 13:02:28 +0100

    Added ClientApi.uploadVersionAsync function

--- WebAPI/frontend-dev/src/scripts/client-api.ts
+++ WebAPI/frontend-dev/src/scripts/client-api.ts
@@ -14,6 +14,30 @@ class ClientApi
 		const json = await response.json();
 		return new ModVersionModel(json);
 	}
+
+	async uploadVersionAsync(file: File, version: string, modType: ModType, releaseDate: Date): Promise<void>
+	{
+		const data = {
+			releaseDate,
+			version
+		};
+
+		const formData = new FormData();
+		formData.set('data', JSON.stringify(data));
+		formData.set('file', file);
+
+		const modTypeString: string = Utility.getFriendlyModType(modType);
+
+		const response = await fetch(`${ClientApi.ApiUrl}/version/${modTypeString}/upload`, {
+			method: 'POST',
+			cache: 'no-cache',
+			credentials: 'same-origin',
+			body: formData
+		});
+
+		if (!response.ok)
+			throw response;
+	}
 }
 
 export default new ClientApi();
