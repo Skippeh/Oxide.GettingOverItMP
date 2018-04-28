6b23d9ef35887f5bb6c79fc399af1ec55a3a593d
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-04-28 13:03:21 +0200

    Removed unnecessary code from ModUI

--- src/Components/ModUI.cs
+++ src/Components/ModUI.cs
@@ -1,25 +1,14 @@
-﻿using System.Data;
-using System.Reflection;
-using Lidgren.Network;
-using ServerShared;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Oxide.GettingOverItMP.Components
 {
     public class ModUI : MonoBehaviour
     {
-        private GameObject localPlayer;
-
-        private PlayerControl control;
-        private Client client;
         private ChatUI chatUi;
         private ServerBrowserUI serverBrowser;
         
         private void Start()
         {
-            localPlayer = GameObject.Find("Player");
-            control = localPlayer.GetComponent<PlayerControl>();
-            client = GameObject.Find("GOIMP.Client").GetComponent<Client>();
             chatUi = gameObject.AddComponent<ChatUI>();
             serverBrowser = gameObject.AddComponent<ServerBrowserUI>();
         }
