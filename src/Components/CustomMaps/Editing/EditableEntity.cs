4ecf4391803814c085f271d33698593978c7223b
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-05-02 23:34:38 +0200

    Show entity icon gizmo when edit mode is enabled (currently same scale as parent object)

--- src/Components/CustomMaps/Editing/EditableEntity.cs
+++ src/Components/CustomMaps/Editing/EditableEntity.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Oxide.GettingOverItMP.Components.CustomMaps.Editing
 {
@@ -6,20 +7,32 @@ namespace Oxide.GettingOverItMP.Components.CustomMaps.Editing
     public class EditableEntity : MonoBehaviour
     {
         private MapEntity entity;
+        private GameObject iconGizmo;
+        private MapEditManager mapEditManager;
 
         private void Awake()
         {
+            mapEditManager = GameObject.FindObjectOfType<MapEditManager>();
+
             entity = GetComponent<MapEntity>();
+            iconGizmo = new GameObject("Icon gizmo");
+            iconGizmo.transform.SetParent(transform, false);
+            iconGizmo.SetActive(mapEditManager.EditModeEnabled);
+
+            var spriteRenderer = iconGizmo.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = MPContent.Assets.LoadAsset<Sprite>("assets/textures/entity-marker.png");
+            spriteRenderer.shadowCastingMode = ShadowCastingMode.Off;
+            spriteRenderer.receiveShadows = false;
         }
 
         public void OnEditModeEnabled()
         {
-
+            iconGizmo.SetActive(true);
         }
 
         public void OnEditModeDisabled()
         {
-
+            iconGizmo.SetActive(false);
         }
     }
 }
